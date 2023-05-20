e6d27110097d16b6f63101f6f2b38b9a281a107e
a5h101 <ak45hhere@gmail.com>
2023-05-20 18:03:13 +0530

    dynamic pug data made

--- controller/viewsController.js
+++ controller/viewsController.js
@@ -1,11 +1,16 @@
+const Tour = require('../models/tourModel');
+
 //exports
-exports.getOverview = (req, res) => {
+exports.getOverview = async (req, res, next) => {
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
 };
 
-exports.getTour = (req, res) => {
+exports.getTour = async (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
   });

65ceef5aac32bacd8a2189f28ced26f255455e00
a5h101 <ak45hhere@gmail.com>
2023-05-20 13:10:51 +0530

    added pug and created view folder

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+const path = require('path');
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
+
+//Global middleware
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -10,11 +16,9 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-// app.use((req, res, next) => {
-//   console.log('hello from the middleware🤣');
-//   req.requestTime = new Date().toISOString();
-//   next();
-// });
+app.get('/', (req, res) => {
+  res.status(200).render('base'); //express will render this req with name base
+});
 
 //Mounting
 app.use('/api/v1/tours', tourRouter);
